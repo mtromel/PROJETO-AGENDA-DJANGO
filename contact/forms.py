ee5ffc580ba396e1914d4af2a429d33d26795be5
Marcos Tromel <marcos.tromel@gmail.com>
2024-09-16 22:52:34 -0300

    aulas ate 16-09-2024

---
+++ contact/forms.py
@@ -0,0 +1,236 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import password_validation
+from . import models
+
+
+class ContactForm(forms.ModelForm):
+    picture = forms.ImageField(
+        widget=forms.FileInput(
+            attrs={
+                'accept': 'image/*',
+            }
+        )
+    )
+    # # dessa forma estou recriando o campo
+    # first_name = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             'class': 'classe-a classe-b',
+    #             'placeholder': 'Escreva aqui'
+    #         }
+    #     ),
+    #     label='Primeiro Nome',
+    #     help_text='Texto de ajuda para seu usuário'
+    # )
+
+    # posso criar um campo que não existe no model
+    # qualquer = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             'class': 'classe-a classe-b',
+    #             'placeholder': 'Escreva aqui'
+    #         }
+    #     ),
+    #     help_text='Texto de ajuda para seu usuário'
+    # )
+
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+
+    #     # dessa forma atualizo o widget que veio no campo
+    #     # self.fields['first_name'].widget.attrs.update({
+    #     #         'class': 'classe-a classe-b',
+    #     #         'placeholder': 'Escreva aqui',
+    #     # })
+
+    class Meta:
+        model = models.Contact
+        fields = (
+            'first_name', 'last_name', 'phone',
+            'email', 'description', 'category', 'picture')
+
+        # dessa forma estou criando um novo widget para o campo
+        # widgets = {
+        #     'first_name': forms.TextInput(
+        #         attrs={
+        #             'class': 'classe-a classe-b',
+        #             'placeholder': 'Escreva aqui'
+        #         }
+        #     )
+        # }
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if first_name == last_name:
+            msg = ValidationError(
+                    'Primeiro nome não pode ser igual ao segundo',
+                    code='invalid')
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+
+        # self.add_error(
+        #     'first_name',
+        #     ValidationError(
+        #         'Mensagem de erro',
+        #         code='invalid'
+        #     )
+        # )
+
+        # self.add_error(
+        #     None,
+        #     ValidationError(
+        #         'Mensagem de erro nonfield',
+        #         code='invalid'
+        #     )
+        # )
+
+        return super().clean()
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Veio do add-error',
+                    code='invalid'
+                )
+            )
+            # raise ValidationError(
+            #     'Não digite ABC neste campo',
+            #     code='invalid'
+            # )
+
+        return first_name
+
+
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    last_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'username', 'password1', 'password2',
+        )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('Já existe um usuário com este e-mail',
+                                code='invalid')
+            )
+
+        return email
+
+
+class RegisterUpdateForm(forms.ModelForm):
+    first_name = forms.CharField(
+        min_length=2,
+        max_length=30,
+        required=True,
+        help_text='Obrigatório.',
+        error_messages={
+            'min_lenght': 'Por favor, insira pelo menos 2 caracteres.'
+        }
+    )
+    last_name = forms.CharField(
+        min_length=2,
+        max_length=30,
+        required=True,
+        help_text='Obrigatório.',
+    )
+    password1 = forms.CharField(
+        label='Senha',
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+        help_text=password_validation.password_validators_help_text_html(),
+        required=False,
+    )
+    password2 = forms.CharField(
+        label='Senha 2',
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+        help_text='Repita a senha inserida acima',
+        required=False,
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'username',
+        )
+
+    def save(self, commit=True):
+        cleaned_data = self.cleaned_data
+        user = super().save(commit=False)
+
+        password = cleaned_data.get('password1')
+
+        if password:
+            user.set_password(password)
+
+        if commit:
+            user.save()
+
+        return user
+
+    def clean(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if password1 or password2:
+            if password1 != password2:
+                self.add_error(
+                    'password2',
+                    ValidationError('Senhas não são iguais')
+                )
+
+        return super().clean()
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        current_email = self.instance.email
+
+        if current_email != email:
+            if User.objects.filter(email=email).exists():
+                self.add_error(
+                    'email',
+                    ValidationError('Já existe um usuário com este e-mail',
+                                    code='invalid')
+                )
+
+        return email
+
+    def clean_password1(self):
+        password1 = self.cleaned_data.get('password1')
+
+        if password1:
+            try:
+                password_validation.validate_password(password1)
+            except ValidationError as errors:
+                self.add_error(
+                    'password1',
+                    ValidationError(errors)
+                )
+
+        return password1
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.utils import timezone  # type: ignore
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Contact(models.Model):
+    class Meta:
+        verbose_name = 'Contato'
+        verbose_name_plural = 'Contatos'
+
+    first_name = models.CharField(max_length=50, verbose_name='Nome')
+    last_name = models.CharField(max_length=50, blank=True,
+                                 verbose_name='Sobrenome')
+    phone = models.CharField(max_length=50, verbose_name='Telefone')
+    email = models.EmailField(max_length=254, blank=True)
+    created_date = models.DateTimeField(default=timezone.now,
+                                        verbose_name='Data de criação')
+    description = models.TextField(blank=True, verbose_name='Descrição')
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/',
+                                verbose_name='Foto')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True, null=True, verbose_name='Categoria'
+        )
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True, verbose_name='Criado por'
+        )
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
